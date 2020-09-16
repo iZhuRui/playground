bbf4691e71c4f5676736b5bc0f0b293bf41017fd
ZhuRui <www.515246872@qq.com>
2020-09-16 19:17:46 +0800

    lesson1

--- MyPlayground.playground/Pages/page数据类型.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/page数据类型.xcplaygroundpage/Contents.swift
@@ -1,6 +1,44 @@
+//常量
 //只赋值一次
+//它的值不要求在编译时期确定，但使用之前必须赋值1次
+//var num = 10
+//num += 10
+//num += 30
+//
+//let age : Int
+//age = num
+
+/*
+//函数
+//func getAge(参数) -> Int(返回类型) {
+//
+//}
+func getAge() -> Int {
+    return 101
+}
+
+let age = getAge()
+print(age)
+*/
+
+/*
+//常量、变量在初始化之前，都不能使用
 let age : Int
-age = 10
+let height : Int
+print(age)
+print(height)
+*/
 
+/*
+//标识符
+ 标识符（如常量名、变量名、函数名）几乎可以使用任何字符
+ 标识符不能以数字开头，不能包含空白字符、制表符、箭头等特殊字符
+ */
 
- 
+var age : Int = 30
+
+print(Int8.max)
+
+let error = (404, "not found")
+print(error.0)
+
