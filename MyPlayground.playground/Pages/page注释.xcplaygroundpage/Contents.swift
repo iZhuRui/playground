f15c0a589167d2917fc8576874559d42c65905ea
ZhuRui <www.515246872@qq.com>
2020-09-16 17:41:24 +0800

    swift

---
+++ MyPlayground.playground/Pages/page注释.xcplaygroundpage/Contents.swift
@@ -0,0 +1,42 @@
+// 单行注释
+
+/*
+ 多行注释
+ /*
+ 多行注释
+ */
+ 
+ markup语法
+ ## 二级标题
+ ### 三级标题
+ 
+ */
+
+//: # 一级标题
+
+/*:
+ # 学习swift
+ ## 基础语法
+ - 变量
+ - 常量
+ ## 笔记
+ > This is a note
+ ---
+ ## 图片
+ ![Logo](mine_accountSetting@3x.png "Local image")
+ ## 面向对象
+ 1. 类
+ 2. 属性
+ 3. 方法
+ ## 汇编分析
+ 
+ ## 粗体/斜体
+ 这是**Bold**，这是*ITALIC*
+ 
+ [苹果官方](https://www.apple.com)
+ */
+
+//markup只在playground中有效
+
+//: [下一页](@next)
+//: [上一页](@previous)
